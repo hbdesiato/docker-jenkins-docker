8331bb876cf8fb719fc303b6e46baa0cae48d2a0
CoreOS Admin <hbdesiato@gmx.de>
2016-11-27 20:19:55 +0000

    change ownership of /var/jenkins_home to jenkins

--- jenkins-docker.sh
+++ jenkins-docker.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 groupadd -g "$(stat -c %g /var/run/docker.sock)" docker
 gpasswd -a jenkins $(stat -c %G /var/run/docker.sock)
+chown jenkins:jenkins "$JENKINS_HOME"
 exec su -c /usr/local/bin/jenkins.sh jenkins
 
